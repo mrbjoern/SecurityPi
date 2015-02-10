432410972754826b8d1345922711ffc07ebad568
mrbjoern <mrbjoern92@gmail.com>
2015-02-10 20:02:38 +0100

    Initial commit

---
+++ src/main/java/com/securitypi/SecurityPiApp.java
@@ -0,0 +1,11 @@
+package com.securitypi;
+
+/**
+ * Created by mrbjoern on 2/10/15.
+ */
+public class SecurityPiApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
